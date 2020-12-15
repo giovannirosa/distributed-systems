91176439dad13022e457d6a80f220031dc11c608
Giovanni Rosa <giovanni_rosa4@hotmail.com>
2020-12-15 19:20:03 -0300

    finish code

--- trab1/src/vring.c
+++ trab1/src/vring.c
@@ -1,6 +1,6 @@
 /*
   Autor: Giovanni Rosa
-  Ultima modificao: 13/12/2020
+  Ultima modificao: 15/12/2020
 
 Programa dedicado a implementacao do VRing como requisito para o Trabalho
 Pratico 1 da disciplina de Sistemas Distribuidos.
@@ -60,6 +60,12 @@ int main(int argc, char *argv[]) {
       puts("\n******************************************");
       printf("Iniciando round de testes %d\n", sim_round);
     }
+    if (token > N - 1) {
+      printf("Um evento foi agendado para o processo %d, mas o maximo de "
+             "processos e de %d!\n",
+             token, N);
+      exit(1);
+    }
     switch (e) {
     case TEST:
       if (status(process[token].id) != 0)
@@ -67,70 +73,41 @@ int main(int argc, char *argv[]) {
       token2 = token;
       printf("\n==========================================\n");
       printf("Iniciando testes do processo %d\n", token);
-      printf("State do processo %d: ", token);
-      for (i = 0; i < N; ++i) {
-        printf("%d[%d] ", i, process[token].state[i]);
-      }
-      puts("");
+      print_state(N, token);
       do {
         token2 = (token2 + 1) % N;
         if (token2 == token) {
           printf("Todos os demais processos estao falhos!\n");
-          exit(1);
+          break;
         }
         t = status(process[token2].id);
         t_result = t % 2 == 0 ? "correto" : "falho";
         printf("Processo %d testou processo %d no tempo %4.1f: %s\n", token,
                token2, time(), t_result);
-        if (t == 0 && process[token].state[token2] % 2 != 0) {
+        if (t == 0 && process[token].state[token2] % 2 != 0 ||
+            t == 1 && process[token].state[token2] % 2 != 1) {
           ++process[token].state[token2];
-          printf("state[%d] atualizado para %d\n", token2,
+          printf("State[%d] atualizado para %d\n", token2,
                  process[token].state[token2]);
-          count_event_test(N, token2, token);
-          count_event_discovery(N, token2, token);
-        } else if (t == 1 && process[token].state[token2] % 2 != 1) {
-          ++process[token].state[token2];
-          printf("state[%d] atualizado para %d\n", token2,
-                 process[token].state[token2]);
-          count_event_test(N, token2, token);
-          count_event_discovery(N, token2, token);
+          count_event_test(N, token, token2);
+          count_event_discovery(N, token, token2);
         }
-        if (t % 2 == 0) { // se par esta sem falha
-          printf(
-              "Atualizando state do processo %d com o state do processo %d\n",
-              token, token2);
-          printf("State do processo %d: ", token2);
-          for (i = 0; i < N; ++i) {
-            printf("%d[%d] ", i, process[token2].state[i]);
-          }
-          puts("");
-          bool transfered = false;
-          for (i = (token2 + 1) % N;; i = (i + 1) % N) {
-            if (i == token2 || i == token)
-              break;
-            if (process[token2].state[i] > process[token].state[i]) {
-              transfered = true;
-              printf("Novidade encontrada, transferindo state[%d]...\n", i);
-              printf("state[%d] atualizado para %d\n", i,
-                     process[token2].state[i]);
-              process[token].state[i] = process[token2].state[i];
-              count_event_discovery(N, i, token);
-            }
-          }
-          if (!transfered) {
-            puts("Nenhuma transferencia realizada");
-          }
+        if (t % 2 == 0) { // se par esta sem falha, verifica novidades
+          check_state(N, token, token2);
         }
       } while (t != 0);
       schedule(TEST, 30.0, token);
-      printf("State do processo %d: ", token);
-      for (i = 0; i < N; ++i) {
-        printf("%d[%d] ", i, process[token].state[i]);
-      }
-      printf("\n==========================================\n");
+      print_state(N, token);
+      printf("==========================================\n");
       break;
     case FAULT:
-      create_event(1, token);
+      // se o evento anterior nao foi diagnosticado ainda, adia o evento para a
+      // proxima rodada
+      if (event != NULL && !event->diag) {
+        delay_event(FAULT, token);
+        break;
+      }
+      create_event(N, 1, token);
       r = request(process[token].id, token, 0);
       if (r != 0) {
         printf("\nNao foi possivel falhar o processo %d\n", token);
@@ -141,7 +118,13 @@ int main(int argc, char *argv[]) {
       }
       break;
     case RECOVERY:
-      create_event(0, token);
+      // se o evento anterior nao foi diagnosticado ainda, adia o evento para a
+      // proxima rodada
+      if (event != NULL && !event->diag) {
+        delay_event(RECOVERY, token);
+        break;
+      }
+      create_event(N, 0, token);
       release(process[token].id, token);
       printf("\n--> Event[%d]: Processo %d recuperou no tempo %4.1f\n",
              event->id, token, time());
@@ -150,29 +133,87 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  puts("\n******************************************");
+  puts("Simulacao encerrada");
+
+  puts("\n==========================================");
+
   print_event_array();
   free_array(event_array);
 
   puts("\n==========================================");
   puts("Programa finalizado com sucesso");
-  puts("Autor: Giovanni Rosa");
+  puts("Autor: Giovanni Rosa :)");
   puts("==========================================");
 }
 
-void count_event_discovery(int N, int p, int q) {
-  if (event != NULL && event->latency == 0 && event->proc == p) {
-    printf("Event[%d] descoberto pelo processo %d\n", event->id, q);
-    ++event->n_proc;
-    if (event->n_proc == N - 1) {
-      printf("Diagnostico do evento %d completo\n", event->id);
+void delay_event(int type, int token) {
+  printf("\n--> O evento agendado para %2.1f foi adiado para %2.1f pois o "
+         "evento anterior nao foi diagnosticado\n",
+         time(), time() + 30.0);
+  schedule(type, 30.0, token);
+}
+
+void print_state(int N, int token) {
+  printf("State do processo %d: ", token);
+  for (int i = 0; i < N; ++i) {
+    printf("%d[%d] ", i, process[token].state[i]);
+  }
+  puts("");
+}
+
+void check_state(int N, int token, int token2) {
+  printf("Atualizando state do processo %d com o state do processo %d\n", token,
+         token2);
+  printf("State do processo %d: ", token2);
+  for (int i = 0; i < N; ++i) {
+    printf("%d[%d] ", i, process[token2].state[i]);
+  }
+  puts("");
+  bool transfered = false;
+  for (int i = (token2 + 1) % N;; i = (i + 1) % N) {
+    if (i == token)
+      break;
+    if (process[token2].state[i] > process[token].state[i]) {
+      transfered = true;
+      printf("Novidade encontrada, transferindo state[%d]...\n", i);
+      printf("State[%d] atualizado para %d\n", i, process[token2].state[i]);
+      process[token].state[i] = process[token2].state[i];
+      count_event_discovery(N, token, i);
+    }
+  }
+  if (!transfered) {
+    puts("Nenhuma transferencia realizada");
+  }
+}
+
+void count_event_discovery(int N, int token, int token2) {
+  // if (event != NULL) {
+  //   printf("Event[%d] = %d %d %d\n", event->id, event->latency, event->proc,
+  //          token2);
+  // }
+  if (event != NULL && event->latency == 0 && event->proc == token2) {
+    printf("Event[%d] descoberto pelo processo %d\n", event->id, token);
+    // checa se todos os processos sem falha descobriram o evento
+    bool diag = true;
+    for (int i = 0; i < N; ++i) {
+      if (process[i].state[token2] != -1 &&
+          process[i].state[token2] != process[token2].state[token2]) {
+        diag = false;
+        break;
+      }
+    }
+    if (diag) {
+      printf("--> Diagnostico do evento %d completo\n", event->id);
+      event->diag = true;
       event->latency = sim_round - event->e_round;
     }
   }
 }
 
-void count_event_test(int N, int p, int q) {
-  if (event != NULL && event->latency == 0 && event->proc == p) {
-    printf("Event[%d] testado pelo processo %d\n", event->id, q);
+void count_event_test(int N, int token, int token2) {
+  if (event != NULL && event->latency == 0 && event->proc == token2) {
+    printf("Event[%d] testado pelo processo %d\n", event->id, token);
     ++event->n_tests;
   }
 }
@@ -183,24 +224,38 @@ void print_event_array() {
   const char *type_str;
   for (Node aux = event_array->ini; aux; aux = aux->next) {
     event = (Event *)aux->cont;
-    type_str = event->type == 0 ? "recuperacao" : "falha";
-    printf("Event[%d] = %s, ocorreu no round %d no processo %d com numero de "
-           "testes de %d e latencia de %d\n",
-           event->id, type_str, event->e_round, event->proc, event->n_tests,
-           event->latency);
+    type_str = event->type == 0 ? "RECUP" : "FALHA";
+    printf("Event[%d]: %s | round %d | tempo %05.1f | processo %d | numero de "
+           "testes de %d | latencia de %d\n",
+           event->id, type_str, event->e_round, event->e_time, event->proc,
+           event->n_tests, event->latency);
   }
 }
 
-void create_event(int type, int process) {
+void create_event(int N, int type, int token) {
   event = (Event *)malloc(sizeof(Event));
   event->id = ++id_cont;
   event->e_round = sim_round;
-  event->proc = process;
+  event->e_time = time();
+  event->proc = token;
   event->type = type;
   event->n_tests = 0;
-  event->n_proc = 0;
+  event->diag = false;
   event->latency = 0;
   insert_array(event_array, event);
+
+  // se falha, reseta o vetor de estados pois é falha crash
+  if (type == 1) {
+    for (int i = (token + 1) % N;; i = (i + 1) % N) {
+      if (i == token)
+        break;
+      process[token].state[i] = -1;
+    }
+  }
+
+  // atualiza o proprio state, apenas para controle do diagnostico, esse valor
+  // nao e usado para transferencia
+  ++process[token].state[token];
 }
 
 void user_input(int *N, int argc, char *argv[]) {
@@ -240,4 +295,6 @@ void schedule_events(int N) {
   }
   schedule(FAULT, 31.0, 1);
   schedule(RECOVERY, 61.0, 1);
+  // schedule(FAULT, 119.0, 3);
+  // schedule(RECOVERY, 189.0, 3);
 }
--- trab1/src/vring.h
+++ trab1/src/vring.h
@@ -1,6 +1,6 @@
 /*
   Autor: Giovanni Rosa
-  Ultima modificao: 14/12/2020
+  Ultima modificao: 15/12/2020
 
 Declaracao do programa VRing
 */
@@ -14,13 +14,15 @@ Declaracao do programa VRing
 #include <stdio.h>
 #include <stdlib.h>
 
-// Eventos
+// Evento de teste
 #define TEST 1
+// Evento de falha
 #define FAULT 2
+// Evento de recuperacao
 #define RECOVERY 3
 
 // Tempo maximo de simulacao
-#define MAX_TIME 150
+#define MAX_TIME 100
 
 // Descritor do processo
 typedef struct {
@@ -28,6 +30,7 @@ typedef struct {
   int *state; // vetor de estados de cada processo
 } ProcessType;
 
+// Vetor de processos da simulacao
 ProcessType *process;
 
 // Controle de ID dos eventos
@@ -35,27 +38,94 @@ int id_cont = 0;
 
 // Descritor do evento
 typedef struct {
-  int id;      // identificador do evento
-  int e_round; // round em que o evento ocorreu
-  int proc;    // numero do processo de origem
-  int type;    // tipo do evento: 0 = recuperacao, 1 = falha
-  int n_tests; // numero de testes do evento
-  int n_proc;  // numero de processos que descobriram o evento
-  int latency; // latencia para diagnosticar o evento
+  int id;        // identificador do evento
+  int e_round;   // round em que o evento ocorreu
+  double e_time; // tempo em que o evento ocorreu
+  int proc;      // numero do processo de origem
+  int type;      // tipo do evento: 0 = recuperacao, 1 = falha
+  int n_tests;   // numero de testes do evento
+  bool diag;     // se o evento ja foi diagnosticado
+  int latency;   // latencia para diagnosticar o evento
 } Event;
 
+// Evento atual da simulacao
 Event *event;
 
+// Vetor de eventos da simulacao
 Array event_array;
 
+// Rodada atual da simulacao
 int sim_round = 0;
 
+/**
+ * Adia evento para a proxima rodada
+ * @param type tipo do evento: 0 = recuperacao, 1 = falha
+ * @param token index do processo em que o evento estava agendado
+ */
+void delay_event(int, int);
+
+/**
+ * Imprime o vetor de estados de um processo especifico
+ * @param token index do processo
+ * @param N numero total de processos
+ */
+void print_state(int, int);
+
+/**
+ * Verifica novidades do vetor de estados de um processo em relacao ao outro
+ * @param N numero total de processos
+ * @param token index do processo testador
+ * @param token2 index do processo testado
+ */
+void check_state(int, int, int);
+
+/**
+ * Contabiliza descoberta do evento por um processo
+ * @param N numero total de processos
+ * @param token index do processo testador
+ * @param token2 index do processo testado
+ */
 void count_event_discovery(int, int, int);
+
+/**
+ * Contabiliza teste do evento por um processo
+ * @param N numero total de processos
+ * @param token index do processo testador
+ * @param token2 index do processo testado
+ */
 void count_event_test(int, int, int);
+
+/**
+ * Imprime vetor com todos os eventos da simulacao
+ */
 void print_event_array();
-void create_event(int, int);
+
+/**
+ * Cria um evento
+ * @param N numero total de processos
+ * @param type tipo do evento: 0 = recuperacao, 1 = falha
+ * @param token index do processo em que o evento ocorreu
+ */
+void create_event(int, int, int);
+
+/**
+ * Processa as entradas do usuario
+ * @param N numero total de processos
+ * @param argc quantidade de argumentos
+ * @param argc vetor de argumentos
+ */
 void user_input(int *, int, char *[]);
+
+/**
+ * Inicializa processos na memoria
+ * @param N numero total de processos
+ */
 void init_process(int);
+
+/**
+ * Agenda os eventos que ocorrerao durante a simulacao
+ * @param N numero total de processos
+ */
 void schedule_events(int);
 
 #endif
